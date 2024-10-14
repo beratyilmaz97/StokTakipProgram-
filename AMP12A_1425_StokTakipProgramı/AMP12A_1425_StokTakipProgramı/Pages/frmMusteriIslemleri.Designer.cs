33826e51816f7abed05f29c88b15445012da5b26
BeratYılmaz97 <yilmazberat1234@gmail.com>
2024-10-14 20:05:51 +0300

    Add files via upload

---
+++ AMP12A_1425_StokTakipProgramı/AMP12A_1425_StokTakipProgramı/Pages/frmMusteriIslemleri.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace AMP12A_1425_StokTakipProgramı
+{
+    partial class MÜŞTERİ_İŞLEMLERİ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdMusteriler = new System.Windows.Forms.DataGridView();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.txtAdres = new System.Windows.Forms.RichTextBox();
+            this.lblAdres = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.txtFirmaAdi = new System.Windows.Forms.TextBox();
+            this.lblFirma = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.mskTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            this.lblBilgi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grdMusteriler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdMusteriler
+            // 
+            this.grdMusteriler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdMusteriler.Location = new System.Drawing.Point(36, 266);
+            this.grdMusteriler.Name = "grdMusteriler";
+            this.grdMusteriler.Size = new System.Drawing.Size(735, 167);
+            this.grdMusteriler.TabIndex = 31;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGuncelle.ImageKey = "png-transparent-firefox-computer-icons-computer-software-patch-update-button-comp" +
+    "uter-network-trademark-computer-wallpaper-thumbnail.png";
+            this.btnGuncelle.Location = new System.Drawing.Point(636, 200);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(86, 23);
+            this.btnGuncelle.TabIndex = 30;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSil.ImageKey = "929430.png";
+            this.btnSil.Location = new System.Drawing.Point(528, 201);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 29;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEkle.ImageKey = "plus-icon-2048x2048-z6v59bd6.png";
+            this.btnEkle.Location = new System.Drawing.Point(436, 200);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 28;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnYeni.ImageKey = "plus-icon-2048x2048-z6v59bd6.png";
+            this.btnYeni.Location = new System.Drawing.Point(33, 201);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(106, 23);
+            this.btnYeni.TabIndex = 27;
+            this.btnYeni.Text = "YENİ KAYIT";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(568, 69);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(203, 96);
+            this.txtAdres.TabIndex = 26;
+            this.txtAdres.Text = "";
+            // 
+            // lblAdres
+            // 
+            this.lblAdres.AutoSize = true;
+            this.lblAdres.Location = new System.Drawing.Point(525, 69);
+            this.lblAdres.Name = "lblAdres";
+            this.lblAdres.Size = new System.Drawing.Size(37, 13);
+            this.lblAdres.TabIndex = 25;
+            this.lblAdres.Text = "Adres:";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(342, 113);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(100, 20);
+            this.txtMail.TabIndex = 24;
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(280, 113);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(29, 13);
+            this.lblMail.TabIndex = 23;
+            this.lblMail.Text = "Mail:";
+            // 
+            // txtFirmaAdi
+            // 
+            this.txtFirmaAdi.Location = new System.Drawing.Point(342, 66);
+            this.txtFirmaAdi.Name = "txtFirmaAdi";
+            this.txtFirmaAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtFirmaAdi.TabIndex = 22;
+            // 
+            // lblFirma
+            // 
+            this.lblFirma.AutoSize = true;
+            this.lblFirma.Location = new System.Drawing.Point(280, 69);
+            this.lblFirma.Name = "lblFirma";
+            this.lblFirma.Size = new System.Drawing.Size(56, 13);
+            this.lblFirma.TabIndex = 21;
+            this.lblFirma.Text = "Firma Adı :";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(92, 66);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 20;
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(33, 113);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(46, 13);
+            this.lblTelefon.TabIndex = 19;
+            this.lblTelefon.Text = "Telefon:";
+            // 
+            // mskTelefon
+            // 
+            this.mskTelefon.Location = new System.Drawing.Point(92, 113);
+            this.mskTelefon.Mask = "(____) _____ - _____";
+            this.mskTelefon.Name = "mskTelefon";
+            this.mskTelefon.Size = new System.Drawing.Size(100, 20);
+            this.mskTelefon.TabIndex = 18;
+            // 
+            // lblAdSoyad
+            // 
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Location = new System.Drawing.Point(33, 69);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(56, 13);
+            this.lblAdSoyad.TabIndex = 17;
+            this.lblAdSoyad.Text = "Ad Soyad:";
+            // 
+            // lblBilgi
+            // 
+            this.lblBilgi.AutoSize = true;
+            this.lblBilgi.Location = new System.Drawing.Point(30, 17);
+            this.lblBilgi.Name = "lblBilgi";
+            this.lblBilgi.Size = new System.Drawing.Size(69, 13);
+            this.lblBilgi.TabIndex = 16;
+            this.lblBilgi.Text = "BİLGİ GİRİŞİ";
+            // 
+            // MÜŞTERİ_İŞLEMLERİ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdMusteriler);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnYeni);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.lblAdres);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.lblMail);
+            this.Controls.Add(this.txtFirmaAdi);
+            this.Controls.Add(this.lblFirma);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.mskTelefon);
+            this.Controls.Add(this.lblAdSoyad);
+            this.Controls.Add(this.lblBilgi);
+            this.Name = "MÜŞTERİ_İŞLEMLERİ";
+            this.Text = "frmMusteriIslemleri";
+            ((System.ComponentModel.ISupportInitialize)(this.grdMusteriler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdMusteriler;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.RichTextBox txtAdres;
+        private System.Windows.Forms.Label lblAdres;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.TextBox txtFirmaAdi;
+        private System.Windows.Forms.Label lblFirma;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.MaskedTextBox mskTelefon;
+        private System.Windows.Forms.Label lblAdSoyad;
+        private System.Windows.Forms.Label lblBilgi;
+    }
+}
---
+++ AMP12A_1425_StokTakipProgramı/AMP12A_1425_StokTakipProgramı/Pages/frmPersonelIslemleri.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace AMP12A_1425_StokTakipProgramı
+{
+    partial class PERSONEL_İŞLEMLERİ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdPersoneller = new System.Windows.Forms.DataGridView();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.cmbYetkı = new System.Windows.Forms.ComboBox();
+            this.lblYetkı = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.RichTextBox();
+            this.lblAdres = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.lblSıfre = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.txtKullanici = new System.Windows.Forms.TextBox();
+            this.lblKullanıcı = new System.Windows.Forms.Label();
+            this.mskTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblBilgi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPersoneller)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdPersoneller
+            // 
+            this.grdPersoneller.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPersoneller.Location = new System.Drawing.Point(47, 293);
+            this.grdPersoneller.Name = "grdPersoneller";
+            this.grdPersoneller.Size = new System.Drawing.Size(745, 150);
+            this.grdPersoneller.TabIndex = 43;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGuncelle.ImageKey = "png-transparent-firefox-computer-icons-computer-software-patch-update-button-comp" +
+    "uter-network-trademark-computer-wallpaper-thumbnail.png";
+            this.btnGuncelle.Location = new System.Drawing.Point(676, 200);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(105, 23);
+            this.btnGuncelle.TabIndex = 42;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSil.ImageKey = "929430.png";
+            this.btnSil.Location = new System.Drawing.Point(583, 201);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 41;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEkle.ImageKey = "plus-icon-2048x2048-z6v59bd6.png";
+            this.btnEkle.Location = new System.Drawing.Point(496, 200);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 40;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnYeni.ImageKey = "plus-icon-2048x2048-z6v59bd6.png";
+            this.btnYeni.Location = new System.Drawing.Point(59, 201);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(102, 23);
+            this.btnYeni.TabIndex = 39;
+            this.btnYeni.Text = "YENİ KAYIT";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            // 
+            // cmbYetkı
+            // 
+            this.cmbYetkı.FormattingEnabled = true;
+            this.cmbYetkı.Location = new System.Drawing.Point(623, 155);
+            this.cmbYetkı.Name = "cmbYetkı";
+            this.cmbYetkı.Size = new System.Drawing.Size(140, 21);
+            this.cmbYetkı.TabIndex = 38;
+            // 
+            // lblYetkı
+            // 
+            this.lblYetkı.AutoSize = true;
+            this.lblYetkı.Location = new System.Drawing.Point(580, 158);
+            this.lblYetkı.Name = "lblYetkı";
+            this.lblYetkı.Size = new System.Drawing.Size(37, 13);
+            this.lblYetkı.TabIndex = 37;
+            this.lblYetkı.Text = "Yetki :";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(623, 62);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(140, 87);
+            this.txtAdres.TabIndex = 36;
+            this.txtAdres.Text = "";
+            // 
+            // lblAdres
+            // 
+            this.lblAdres.AutoSize = true;
+            this.lblAdres.Location = new System.Drawing.Point(580, 65);
+            this.lblAdres.Name = "lblAdres";
+            this.lblAdres.Size = new System.Drawing.Size(37, 13);
+            this.lblAdres.TabIndex = 35;
+            this.lblAdres.Text = "Adres:";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(365, 151);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(129, 20);
+            this.txtSifre.TabIndex = 34;
+            // 
+            // lblSıfre
+            // 
+            this.lblSıfre.AutoSize = true;
+            this.lblSıfre.Location = new System.Drawing.Point(319, 151);
+            this.lblSıfre.Name = "lblSıfre";
+            this.lblSıfre.Size = new System.Drawing.Size(31, 13);
+            this.lblSıfre.TabIndex = 33;
+            this.lblSıfre.Text = "Şifre:";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(365, 107);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(129, 20);
+            this.txtMail.TabIndex = 32;
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(319, 110);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(29, 13);
+            this.lblMail.TabIndex = 31;
+            this.lblMail.Text = "Mail:";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(365, 68);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(129, 20);
+            this.txtSoyad.TabIndex = 30;
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(316, 71);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(43, 13);
+            this.lblSoyad.TabIndex = 29;
+            this.lblSoyad.Text = "Soyad :";
+            // 
+            // txtKullanici
+            // 
+            this.txtKullanici.Location = new System.Drawing.Point(133, 144);
+            this.txtKullanici.Name = "txtKullanici";
+            this.txtKullanici.Size = new System.Drawing.Size(111, 20);
+            this.txtKullanici.TabIndex = 28;
+            // 
+            // lblKullanıcı
+            // 
+            this.lblKullanıcı.AutoSize = true;
+            this.lblKullanıcı.Location = new System.Drawing.Point(56, 147);
+            this.lblKullanıcı.Name = "lblKullanıcı";
+            this.lblKullanıcı.Size = new System.Drawing.Size(70, 13);
+            this.lblKullanıcı.TabIndex = 27;
+            this.lblKullanıcı.Text = "Kullanıcı Adı :";
+            // 
+            // mskTelefon
+            // 
+            this.mskTelefon.Location = new System.Drawing.Point(133, 107);
+            this.mskTelefon.Mask = "(____) _____ - _____";
+            this.mskTelefon.Name = "mskTelefon";
+            this.mskTelefon.Size = new System.Drawing.Size(111, 20);
+            this.mskTelefon.TabIndex = 26;
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(80, 107);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(46, 13);
+            this.lblTelefon.TabIndex = 25;
+            this.lblTelefon.Text = "Telefon:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(133, 65);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(111, 20);
+            this.txtAd.TabIndex = 24;
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(100, 65);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(26, 13);
+            this.lblAd.TabIndex = 23;
+            this.lblAd.Text = "Ad :";
+            // 
+            // lblBilgi
+            // 
+            this.lblBilgi.AutoSize = true;
+            this.lblBilgi.Location = new System.Drawing.Point(31, 23);
+            this.lblBilgi.Name = "lblBilgi";
+            this.lblBilgi.Size = new System.Drawing.Size(69, 13);
+            this.lblBilgi.TabIndex = 22;
+            this.lblBilgi.Text = "BİLGİ GİRİŞİ";
+            // 
+            // PERSONEL_İŞLEMLERİ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 467);
+            this.Controls.Add(this.grdPersoneller);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnYeni);
+            this.Controls.Add(this.cmbYetkı);
+            this.Controls.Add(this.lblYetkı);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.lblAdres);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.lblSıfre);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.lblMail);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.txtKullanici);
+            this.Controls.Add(this.lblKullanıcı);
+            this.Controls.Add(this.mskTelefon);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.lblBilgi);
+            this.Name = "PERSONEL_İŞLEMLERİ";
+            this.Text = "frmPersonelIslemleri";
+            ((System.ComponentModel.ISupportInitialize)(this.grdPersoneller)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdPersoneller;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.ComboBox cmbYetkı;
+        private System.Windows.Forms.Label lblYetkı;
+        private System.Windows.Forms.RichTextBox txtAdres;
+        private System.Windows.Forms.Label lblAdres;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label lblSıfre;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.TextBox txtKullanici;
+        private System.Windows.Forms.Label lblKullanıcı;
+        private System.Windows.Forms.MaskedTextBox mskTelefon;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblBilgi;
+    }
+}
